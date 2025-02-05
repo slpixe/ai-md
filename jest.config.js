8a89545ce412f172c06affc5e2c6f345ae8bc6ee
Dean Quinney <slpixe@gmail.com>
2025-02-05 02:29:45 +0100

    vitest true is true works

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: "node",
-  transform: {
-    "^.+\\.tsx?$": ["ts-jest", {}],
-  },
-  moduleDirectories: ["node_modules", "src"],
-};
